e145ec930b47b11274bb17badc9d7cb2de448da6
sujatha <sujatha2855@gmail.com>
2024-10-27 22:26:21 +0530

    this is my prime number program

---
+++ src/test/java/InterviewQuations/PrimeNumber.java
@@ -0,0 +1,28 @@
+package InterviewQuations;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter the number");
+        int number =sc.nextInt();
+
+
+            if(number%2==1  )
+            {
+                System.out.println(" its  a prime number");
+            }
+            else
+            {
+                System.out.println(" its not a prime number");
+            }
+
+
+
+
+        }
+
+
+    }
+
